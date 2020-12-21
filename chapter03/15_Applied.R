647185e826298f067930931ab1a5db7d104f6f7b
Taraqur Rahman <tqrahman8@gmail.com>
2020-12-21 11:53:50 -0500

    cleaned 15

--- chapter03/15_Applied.R
+++ chapter03/15_Applied.R
@@ -1,6 +1,7 @@
-#library(ISLR)
+# Reading in libraries
 library(MASS)
 
+# Attaching the Boston dataset to workspace
 attach(Boston)
 
 lm.function = function(predictor) {
@@ -14,35 +15,73 @@ lm.function = function(predictor) {
 # for (v in c(rm, age)) {
 #   summary(lm(crim ~ v, data = Boston))
 # }
-lm.function(rm)
-lm.zn = lm(crim ~ zn, data = Boston)
-lm.indus = lm(crim ~ indus, data = Boston)
-lm.chas = lm(crim ~ chas, data = Boston)
-lm.nox = lm(crim ~ nox, data = Boston)
-lm.rm = lm(crim ~ rm, data = Boston)
-lm.age = lm(crim ~ age, data = Boston)
-lm.dis = lm(crim ~ dis, data = Boston)
-lm.rad = lm(crim ~ rad, data = Boston)
-lm.tax = lm(crim ~ tax, data = Boston)
-lm.ptratio = lm(crim ~ ptratio, data = Boston)
-lm.lstat = lm(crim ~ lstat, data = Boston)
-lm.medv = lm(crim ~ medv, data = Boston)
+# lm.function(rm)
 
+# a ####
+
+# Creating a SLR model to predict crim using zn
+lm.zn = lm(crim ~ zn, data = Boston)
 summary(lm.zn)
-par(mfrow = c(2,2)) # 4 plots in same picture
+# Based on the p-value (5.51e-6), zn has a significant association with crim
+
+# Plotting diagnostic plots
+par(mfrow = c(2,2))
 plot(lm.zn)
-plot(lm.zn, Boston$zn)
+
+# Creating a SLR model to predict crim using indus
+lm.indus = lm(crim ~ indus, data = Boston)
 summary(lm.indus)
+# Based on the p-value (2e-16), indus has a significant association with crim
+
+# Creating a SLR model to predict crim using chas
+lm.chas = lm(crim ~ chas, data = Boston)
 summary(lm.chas)
+# Based on the p-value (.209), chas does not have an association with crim
+
+# Creating a SLR model to predict crim using nox
+lm.nox = lm(crim ~ nox, data = Boston)
 summary(lm.nox)
+# Based on the p-value (2e-16), nox has a significant association with crim
+
+# Creating a SLR model to predict crim using rm
+lm.rm = lm(crim ~ rm, data = Boston)
 summary(lm.rm)
+# Based on the p-value (6.35e-7), rn has a significant association with crim
+
+# Creating a SLR model to predict crim using age
+lm.age = lm(crim ~ age, data = Boston)
 summary(lm.age)
+# Based on the p-value (2.85e-16), age has a significant association with crim
+
+# Creating a SLR model to predict crim using dis
+lm.dis = lm(crim ~ dis, data = Boston)
 summary(lm.dis)
+# Based on the p-value (2e-16), dis has a significant association with crim
+
+# Creating a SLR model to predict crim using rad
+lm.rad = lm(crim ~ rad, data = Boston)
 summary(lm.rad)
+# Based on the p-value (2e-16), rad has a significant association with crim
+
+# Creating a SLR model to predict crim using tax
+lm.tax = lm(crim ~ tax, data = Boston)
 summary(lm.tax)
+# Based on the p-value (2e-16), tax has a significant association with crim
+
+# Creating a SLR model to predict crim using ptratio
+lm.ptratio = lm(crim ~ ptratio, data = Boston)
 summary(lm.ptratio)
+# Based on the p-value (2.94e-11), ptratio has a significant association with crim
+
+# Creating a SLR model to predict crim using lstat
+lm.lstat = lm(crim ~ lstat, data = Boston)
 summary(lm.lstat)
+# Based on the p-value (2e-16), lstat has a significant association with crim
+
+# Creating a SLR model to predict crim using medv
+lm.medv = lm(crim ~ medv, data = Boston)
 summary(lm.medv)
+# Based on the p-value (2e-16), tax has a significant association with crim
 
 plot(lm.indus)
 plot(lm.chas)
@@ -77,10 +116,17 @@ y = coefficients(lm.all)[2:14]
 plot(x, y)
 
 # x$coeffients <-  c('Intercept', "RME")
+
 # b ####
+
 # Fitting a multiple linear regression
 mlr.fit = lm(crim ~ ., data=Boston)
 
 # Viewing the statistics for the MLR model
 summary(mlr.fit)
+
+# c ####
+
+
+# d ####
 
