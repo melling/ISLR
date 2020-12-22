f0cd9c83630ebc3817a6971c998135a391377c15
Michael Mellinger <mmellinger66@gmail.com>
2020-12-22 17:40:42 -0500

    Added explicit table names

--- chapter04/04_10_melling.R
+++ chapter04/04_10_melling.R
@@ -5,12 +5,12 @@ library(tidyverse)
 
 #View(Weekly)
 
-w2 = tibble(Weekly)
-summary(w2)
+# w2 = tibble(Weekly)
+# summary(w2)
 
 # 10a ####
 summary(Weekly)
-attach(Weekly)
+# attach(Weekly)
 pairs(Weekly)
 
 # 10b: Logistic Regression ####
@@ -19,6 +19,7 @@ names(Weekly)
 glm.fit1 = glm(Direction ~ Lag1+Lag2+Lag3+Lag4+Lag5+Volume, data = Weekly, family = binomial)  # binomial for logistic regression
 summary(glm.fit1)    
 
+# Answer
 # Lag2, other predictors appear statistically significant: All Pr(>|z|) > 0.05
 
 
@@ -26,7 +27,7 @@ summary(glm.fit1)
 
 glm.probs = predict(glm.fit1, type = "response")
 #View(glm.probs)
-contrasts(Direction)
+contrasts(Weekly$Direction)
 
 ### Confusion Matrix ####
 # Create prediction table
@@ -40,7 +41,7 @@ summary(glm.pred)
 
 # Produce a confusion matrix
 # Diagonals indicate correct predictions. Off-diagonals indicate incorrect predictions.
-table(glm.pred, Direction) 
+table(glm.pred, Weekly$Direction) 
 
 # glm.p Down  Up
 # Down   54  48
@@ -53,14 +54,14 @@ table(glm.pred, Direction)
 430/1809
 48/1809
 (430 + 557)/1806
-mean(glm.pred == Direction) ## Correct
-mean(glm.pred != Direction) ## Incorrect
+mean(glm.pred == Weekly$Direction) ## Correct
+mean(glm.pred != Weekly$Direction) ## Incorrect
 
 # Saying Up 430 times when it shouldn't?
 
 # 10d: ####
 summary(Weekly$Year)
-train = (Year<2009)
+train = (Weekly$Year<2009)
 weekly.2009 = Weekly[!train,]
 
 Direction.2009 = Weekly$Direction[!train]
