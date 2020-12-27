bc0b2f75f0b16de336571d8bc3569c0459c757f5
Michael Mellinger <mmellinger66@gmail.com>
2020-12-27 15:53:18 -0500

    Last part of lab

--- chapter05/05_Lab_melling.R
+++ chapter05/05_Lab_melling.R
@@ -101,8 +101,33 @@ boot(Portfolio, alpha.fn, R=1000)
 
 # Estimating the Accuracy of a Linear Regression Model ####
 
-boot.fn=function(data,index)
-  return(coef(lm(mpg∼horsepower ,data=data,subset=index))) > boot.fn(Auto ,1:392)
+boot.fn=function(data,index) return(coef(lm(mpg~horsepower ,data=data,subset=index)))
+
+boot.fn(Auto ,1:392)
+
+
+set.seed(1)
+boot.fn(Auto,sample(392,392,replace=T))
+
+boot.fn(Auto,sample(392,392,replace=T))
+
+
+boot(Auto, boot.fn, 1000)
+
+summary(lm(mpg~horsepower ,data=Auto))$coef
+
+# Another boot fn ####
+
+boot.fn=function(data,index) coefficients(lm(mpg~horsepower+I(horsepower^2),data=data, subset=index))
+
+
+set.seed(1)
+boot(Auto, boot.fn, 1000)
+
+summary(lm(mpg~horsepower+I(horsepower^2),data=Auto))$coef
+
+
+#
 
 
 detach(Auto)
