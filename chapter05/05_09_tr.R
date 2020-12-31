a149fba8ce9a19794826d48ccfc19f2b050d4007
Taraqur Rahman <tqrahman8@gmail.com>
2020-12-31 13:44:20 -0500

    finished 5.9

--- chapter05/05_09_tr.R
+++ chapter05/05_09_tr.R
@@ -39,3 +39,39 @@ conf.int = c(bstrap$t0 - (2 * 0.3994119), bstrap$t0 + (2  * 0.3994119))
 
 # 
 t.test(Boston$medv)
+
+#### 9(e) ####
+
+median_hat = median(Boston$medv)
+print(median_hat) # 21.2
+
+#### 9(f) ####
+
+# Calculating median of data
+boot.median.fn = function(data, index) {
+  return(median(data[index]))
+}
+
+# Running bootstrap for median
+bstrap.median = boot(Boston$medv, boot.median.fn, 10000)
+print(bstrap.median) # 0.3754
+
+## Comments: Median is the same, whether applying boostrap or just median(). The SE, 0.38,
+## is small compared to the median value of 21.2.
+
+#### 9(g) ####
+
+quant.ten = quantile(Boston$medv, probs=c(.1)) # 12.75 
+
+#### 9(h) ####
+
+# Calculating 10%  quantile  of data
+boot.quant.ten.fn = function(data, index) {
+  return(quantile(data[index], probs=c(.1)))
+}
+
+# Running bootstrap for median
+boot.quant.ten = boot(Boston$medv, boot.quant.ten.fn, 10000)
+print(boot.quant.ten) # SE: 0.5027
+
+## Comments: There was no difference with the 10% quartile, There was a SE of .50.
