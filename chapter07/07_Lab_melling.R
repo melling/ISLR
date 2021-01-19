2df6c94f09e4585e0618e3a0c31ff26d3aac1780
Michael Mellinger <mmellinger66@gmail.com>
2021-01-19 13:06:01 -0500

    Lab 7 Smooth Splines

--- chapter07/07_Lab_melling.R
+++ chapter07/07_Lab_melling.R
@@ -125,6 +125,8 @@ coef(summary(fit))
 # p293
 
 # Basic functions
+# B-spline or basis spline
+#
 fit = lm(wage ~ bs(age, knots=c(25,40,60)), data = Wage)
 pred = predict(fit, newdata=list(age=age.grid), se=T)
 
@@ -133,3 +135,40 @@ plot(Wage$age,Wage$wage, col="gray")
 lines(age.grid, pred$fit, lwd=2)
 lines(age.grid, pred$fit + 2 * pred$se , lty="dashed")
 lines(age.grid, pred$fit - 2 * pred$se , lty="dashed")
+
+dim(bs(Wage$age, knots = c(25,40,60)))
+#bs(Wage$age, knots = c(25,40,60))
+dim(bs(Wage$age, df=6)) # 7 Degrees of freedom, 6 basis functions
+attr(bs(Wage$age, df=6), "knots")
+
+# Natural Spline ####
+fit2=lm(wage~ns(age,df=4), data = Wage)
+pred2=predict(fit2, newdata=list(age=age.grid), se=T)
+lines(age.grid, pred2$fit, col="red", lwd=2)
+
+plot(Wage$age, Wage$wage, xlim = agelims, cex=.5, col="darkgrey")
+title("Smoothing Spline")
+fit= smooth.spline(Wage$age, Wage$wage, df=16)
+fit2=smooth.spline(Wage$age, Wage$wage, cv = TRUE)
+fit2$df
+
+lines(fit, col = "red", lwd=2)
+lines(fit2, col = "blue", lwd=2)
+legend("topright", legend=c("16 DF", "6.8 DF"),
+       col=c("red", "blue"), lty=1, lwd = 2, cex=.8)
+
+# Local Regression ####
+# Uses loess
+attach(Wage)
+plot(age, wage, xlim = agelims, cex=.5, col="darkgrey")
+title("Local Regression")
+
+fit=loess(wage~age, span=.2,data=Wage)
+fit2=loess(wage~age, span=.5,data=Wage)
+
+lines(age.grid, predict(fit,data.frame(age=age.grid)),
+      col="red", lwd=2)
+lines(age.grid, predict(fit2,data.frame(age=age.grid)),
+      col="blue", lwd=2)
+legend("topright", legend=c("Span=0.2", "Span=0.5"),
+       col=c("red", "blue"), lty=1, lwd = 2, cex=.8)
