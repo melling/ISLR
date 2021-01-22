9c4785ab0cde859f53501d4033935f47e3a6bc35
Michael Mellinger <mmellinger66@gmail.com>
2021-01-22 12:26:23 -0500

    Finished GAM part of Lab 7

--- chapter07/07_Lab_melling.R
+++ chapter07/07_Lab_melling.R
@@ -177,5 +177,38 @@ legend("topright", legend=c("Span=0.2", "Span=0.5"),
 #install.packages("akima")
 #install.packages("gam")
 
+gam1=lm(wage~ns(year,4)+ns(age,5) + education, data=Wage)
+
 library(gam)
+
+gam.m3=gam(wage~s(year,4) + s(age,5) + education, data=Wage)
+par(mfrow=c(1,3))
+plot(gam.m3, se=TRUE,col="blue")
+
+plot.gam(gam1, se=TRUE, col="red")
+
+
+gam.m1=gam(wage~s(age,5) + education, data=Wage)
+gam.m2=gam(wage~year+s(age,5) + education, data=Wage)
+anova(gam.m1,gam.m2,gam.m3,test="F")
+
+
+summary(gam.m3)
+
+preds=predict(gam.m2,newdata=Wage)
+
+gam.lo=gam(wage~s(year,df=4)+lo(age,span=0.7)+education, data=Wage)
+plot.gam(gam.lo, se=TRUE, col="green")
+
+
 library(akima)
+plot(gam.lo.i)
+
+gam.lr=gam(I(wage>250)~year+s(age,df=5)+education, family=binomial,data=Wage)
+par(mfrow=c(1,3))
+plot(gam.lr,se=T,col="green")
+
+table(education, I(wage >250))
+
+gam.lr.s=gam(I(wage>250) ~ year+s(age,df=5)+education, family=binomial, data=Wage, subset=(education!="1. < HS Grad"))
+plot(gam.lr.s,se=T,col="green")
