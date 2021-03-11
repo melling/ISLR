2a2257edafa7d229810f1186636cb13f1cd15bf6
Taraqur Rahman <tqrahman8@gmail.com>
2021-03-11 00:46:10 -0500

    added notes to 3.9

--- chapter03/09_Applied.R
+++ chapter03/09_Applied.R
@@ -4,9 +4,16 @@ library(ISLR)
 
 # (a) 
 pairs(Auto)
+'Horsepower and weight is highly correlated. (RED BOX)
+ Displacement and weight are highly correlated. (GREEN BOX)
+ MPG and acceleration seems like a non linear relationship (BLUE BOX)
+'
+
 
 # (b)
 cor(Auto[,!colnames(Auto) %in% c("name")])
+' Numerically view the correlation
+'
 
 # (c)
 # Running a MLR on all predictors except for name
@@ -15,14 +22,19 @@ summary(auto.mlr)
 
 # i.
 # There are multiple predictors that have relationship with the response 
-# because their associated p-value is significant
+# because their associated p-value is significant. The p-value tells
+# us the probability that the coefficient will take a value of 0. The
+# typical threshold for p-value is 0.05. If the probability is below 
+# 0.05, then that means chances that it will be 0 is very slim.
 
 # ii.
 # The predictors: displacement, weight, year, and origin have a 
 # statistically significant relationship.
 
 # iii.
-# The coefficient of year suggests that every 4 years, the mpg goes up by 3 
+# The coefficient of year is 0.7507 which is about 3/4. This tells us 
+# the relationship between year and MPG. It suggests that every 3 years,
+# the mpg goes up by 4. 
 
 # (d)
 par(mfrow=c(2,2))
@@ -34,12 +46,19 @@ plot(auto.mlr)
 # Non-constant Variance: The residual plot also shows that the variance is not constant. There 
 # is a funnel shape appearing at the end which indicates heteroscedasticity (non-constant variance)
 
+# Normal Q-Q Plot shows that the residuals are normally distributed if 
+# the observations line up on the dashed line. In this case majority of 
+# the obeervations lie on the line except for 323, 327, 326.
+
 # Outliers: There seems to not be any outliers because in the  Scale-Location, all values are within 
-# the range of [-2,2]. It will only be an outlier if standardized residual is outside the range of 
+# the range of [-2,2]. It will only be an outlier if studentized residual is outside the range of 
 # [-3, 3].
 
 # High Leverage Points: Based on the Residuals vs. Leverage graph, there is no observations that
-# provides a high leverage
+# provides a high leverage. To determine if observations contains high leverage, 
+# we will have to look to see if there are any points above the red dotted line.
+# If there is then that observation has high leverage. In this case, there 
+# are no high leverage observations.
 
 # (e) ####
 
